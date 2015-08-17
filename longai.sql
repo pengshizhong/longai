6e31beddf200c79a8a14c5622095b4bbaa013418
pengshizhong <201231000918@fake.git.javaoop.sweelia.com>
2015-08-17 19:46:43 +0800

    Ôö¼ÓÁËÐÅϢ£¬ÐԱ𣬻ÀïÃæ״̬µÄö¾ÙÀàÐÍ

--- longai.sql
+++ longai.sql
@@ -1,195 +1,318 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2015/8/17 16:38:11                           */
-/*==============================================================*/
+CREATE DATABASE  IF NOT EXISTS `longai` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `longai`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: longai
+-- ------------------------------------------------------
+-- Server version	5.6.15
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-drop table if exists enroll;
+--
+-- Table structure for table `enroll`
+--
 
-drop table if exists event;
+DROP TABLE IF EXISTS `enroll`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enroll` (
+  `enroll_sid` int(11) NOT NULL AUTO_INCREMENT,
+  `event_sid` int(11) DEFAULT NULL,
+  `user_sid` int(11) DEFAULT NULL,
+  `enroll_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`enroll_sid`),
+  KEY `FK_Relationship_2` (`user_sid`),
+  KEY `FK_Relationship_3` (`event_sid`),
+  CONSTRAINT `FK_Relationship_3` FOREIGN KEY (`event_sid`) REFERENCES `event` (`event_sid`),
+  CONSTRAINT `FK_Relationship_2` FOREIGN KEY (`user_sid`) REFERENCES `user` (`user_sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-drop table if exists message;
+--
+-- Dumping data for table `enroll`
+--
 
-drop table if exists moment;
+LOCK TABLES `enroll` WRITE;
+/*!40000 ALTER TABLE `enroll` DISABLE KEYS */;
+/*!40000 ALTER TABLE `enroll` ENABLE KEYS */;
+UNLOCK TABLES;
 
-drop table if exists momentcomment;
+--
+-- Table structure for table `event`
+--
 
-drop table if exists momentlike;
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `event_sid` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` varchar(10) DEFAULT NULL,
+  `title` varchar(20) DEFAULT NULL,
+  `sponsors` varchar(30) DEFAULT NULL,
+  `starttime` datetime DEFAULT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `status` enum('READY','START','END') DEFAULT NULL,
+  `contenturl` varchar(200) DEFAULT NULL,
+  `permission` varchar(10) DEFAULT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`event_sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-drop table if exists news;
+--
+-- Dumping data for table `event`
+--
 
-drop table if exists newscomment;
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
 
-drop table if exists newslike;
+--
+-- Table structure for table `message`
+--
 
-drop table if exists user;
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `message_sid` int(11) NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(10) DEFAULT NULL,
+  `from` varchar(30) DEFAULT NULL,
+  `to` varchar(30) DEFAULT NULL,
+  `status` enum('FAILED','SENDING','SUCCEED') DEFAULT NULL,
+  `sendtime` datetime DEFAULT NULL,
+  `receivetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`message_sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*==============================================================*/
-/* Table: enroll                                                */
-/*==============================================================*/
-create table enroll
-(
-   enroll_sid           int not null auto_increment,
-   event_sid            int,
-   user_sid             int,
-   enroll_id            int,
-   primary key (enroll_sid)
-);
+--
+-- Dumping data for table `message`
+--
 
-/*==============================================================*/
-/* Table: event                                                 */
-/*==============================================================*/
-create table event
-(
-   event_sid            int not null auto_increment,
-   event_id             varchar(10),
-   title                varchar(20),
-   sponsors             varchar(30),
-   starttime            datetime,
-   endtime              datetime,
-   status               varchar(20),
-   contenturl           varchar(200),
-   permission           varchar(10),
-   location             varchar(200),
-   primary key (event_sid)
-);
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*==============================================================*/
-/* Table: message                                               */
-/*==============================================================*/
-create table message
-(
-   message_sid          int not null auto_increment,
-   message_id           varchar(10),
-   "from"               varchar(30),
-   to                   varchar(30),
-   status               varchar(20),
-   sendtime             datetime,
-   receivetime          datetime,
-   primary key (message_sid)
-);
+--
+-- Table structure for table `moment`
+--
 
-/*==============================================================*/
-/* Table: moment                                                */
-/*==============================================================*/
-create table moment
-(
-   moment_id            int not null auto_increment,
-   moment_content       varchar(200),
-   moment_pubtime       datetime,
-   primary key (moment_id)
-);
+DROP TABLE IF EXISTS `moment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moment` (
+  `moment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `moment_content` varchar(200) DEFAULT NULL,
+  `moment_pubtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`moment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*==============================================================*/
-/* Table: momentcomment                                         */
-/*==============================================================*/
-create table momentcomment
-(
-   momentcomment_id     int not null auto_increment,
-   user_sid             int,
-   moment_id            int,
-   momentcommentcontent varchar(200),
-   momentcontentpubtime datetime,
-   primary key (momentcomment_id)
-);
+--
+-- Dumping data for table `moment`
+--
 
-/*==============================================================*/
-/* Table: momentlike                                            */
-/*==============================================================*/
-create table momentlike
-(
-   moment_id            int,
-   user_sid             int,
-   momentlike_id        int not null auto_increment,
-   momentlikepubtime    datetime,
-   primary key (momentlike_id)
-);
+LOCK TABLES `moment` WRITE;
+/*!40000 ALTER TABLE `moment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*==============================================================*/
-/* Table: news                                                  */
-/*==============================================================*/
-create table news
-(
-   news_id              int not null auto_increment,
-   content              varchar(1000),
-   auther               varchar(20),
-   title                varchar(20),
-   primary key (news_id)
-);
+--
+-- Table structure for table `momentcomment`
+--
 
-/*==============================================================*/
-/* Table: newscomment                                           */
-/*==============================================================*/
-create table newscomment
-(
-   newscomment_id       int not null auto_increment,
-   news_id              int,
-   user_sid             int,
-   comment_contet       varchar(200),
-   pubtime              datetime,
-   primary key (newscomment_id)
-);
+DROP TABLE IF EXISTS `momentcomment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `momentcomment` (
+  `momentcomment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_sid` int(11) DEFAULT NULL,
+  `moment_id` int(11) DEFAULT NULL,
+  `momentcommentcontent` varchar(200) DEFAULT NULL,
+  `momentcontentpubtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`momentcomment_id`),
+  KEY `FK_Relationship_6` (`user_sid`),
+  KEY `FK_Relationship_9` (`moment_id`),
+  CONSTRAINT `FK_Relationship_9` FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`),
+  CONSTRAINT `FK_Relationship_6` FOREIGN KEY (`user_sid`) REFERENCES `user` (`user_sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*==============================================================*/
-/* Table: newslike                                              */
-/*==============================================================*/
-create table newslike
-(
-   newslike_id          int not null auto_increment,
-   user_sid             int,
-   news_id              int,
-   newslike_pubtime     char(10),
-   primary key (newslike_id)
-);
+--
+-- Dumping data for table `momentcomment`
+--
 
-/*==============================================================*/
-/* Table: user                                                  */
-/*==============================================================*/
-create table user
-(
-   name                 varchar(50),
-   nickname             varchar(50),
-   email                varchar(100),
-   location             varchar(200),
-   biography            varchar(200),
-   profilepic           varchar(200),
-   gender               varchar(10),
-   type                 varchar(20),
-   preferencegens       double,
-   selfgens             double,
-   Attribute_51         char(10),
-   user_sid             int not null auto_increment,
-   user_id              varchar(10),
-   primary key (user_sid)
-);
+LOCK TABLES `momentcomment` WRITE;
+/*!40000 ALTER TABLE `momentcomment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `momentcomment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-alter table enroll add constraint FK_Relationship_2 foreign key (user_sid)
-      references user (user_sid) on delete restrict on update restrict;
+--
+-- Table structure for table `momentlike`
+--
 
-alter table enroll add constraint FK_Relationship_3 foreign key (event_sid)
-      references event (event_sid) on delete restrict on update restrict;
+DROP TABLE IF EXISTS `momentlike`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `momentlike` (
+  `moment_id` int(11) DEFAULT NULL,
+  `user_sid` int(11) DEFAULT NULL,
+  `momentlike_id` int(11) NOT NULL AUTO_INCREMENT,
+  `momentlikepubtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`momentlike_id`),
+  KEY `FK_Relationship_7` (`user_sid`),
+  KEY `FK_Relationship_8` (`moment_id`),
+  CONSTRAINT `FK_Relationship_8` FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`),
+  CONSTRAINT `FK_Relationship_7` FOREIGN KEY (`user_sid`) REFERENCES `user` (`user_sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-alter table momentcomment add constraint FK_Relationship_6 foreign key (user_sid)
-      references user (user_sid) on delete restrict on update restrict;
+--
+-- Dumping data for table `momentlike`
+--
 
-alter table momentcomment add constraint FK_Relationship_9 foreign key (moment_id)
-      references moment (moment_id) on delete restrict on update restrict;
+LOCK TABLES `momentlike` WRITE;
+/*!40000 ALTER TABLE `momentlike` DISABLE KEYS */;
+/*!40000 ALTER TABLE `momentlike` ENABLE KEYS */;
+UNLOCK TABLES;
 
-alter table momentlike add constraint FK_Relationship_7 foreign key (user_sid)
-      references user (user_sid) on delete restrict on update restrict;
+--
+-- Table structure for table `news`
+--
 
-alter table momentlike add constraint FK_Relationship_8 foreign key (moment_id)
-      references moment (moment_id) on delete restrict on update restrict;
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(1000) DEFAULT NULL,
+  `auther` varchar(20) DEFAULT NULL,
+  `title` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`news_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-alter table newscomment add constraint FK_Relationship_10 foreign key (news_id)
-      references news (news_id) on delete restrict on update restrict;
+--
+-- Dumping data for table `news`
+--
 
-alter table newscomment add constraint FK_Relationship_4 foreign key (user_sid)
-      references user (user_sid) on delete restrict on update restrict;
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
 
-alter table newslike add constraint FK_Relationship_11 foreign key (news_id)
-      references news (news_id) on delete restrict on update restrict;
+--
+-- Table structure for table `newscomment`
+--
 
-alter table newslike add constraint FK_Relationship_5 foreign key (user_sid)
-      references user (user_sid) on delete restrict on update restrict;
+DROP TABLE IF EXISTS `newscomment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `newscomment` (
+  `newscomment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) DEFAULT NULL,
+  `user_sid` int(11) DEFAULT NULL,
+  `comment_contet` varchar(200) DEFAULT NULL,
+  `pubtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`newscomment_id`),
+  KEY `FK_Relationship_10` (`news_id`),
+  KEY `FK_Relationship_4` (`user_sid`),
+  CONSTRAINT `FK_Relationship_4` FOREIGN KEY (`user_sid`) REFERENCES `user` (`user_sid`),
+  CONSTRAINT `FK_Relationship_10` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `newscomment`
+--
+
+LOCK TABLES `newscomment` WRITE;
+/*!40000 ALTER TABLE `newscomment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newscomment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `newslike`
+--
+
+DROP TABLE IF EXISTS `newslike`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `newslike` (
+  `newslike_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_sid` int(11) DEFAULT NULL,
+  `news_id` int(11) DEFAULT NULL,
+  `newslike_pubtime` char(10) DEFAULT NULL,
+  PRIMARY KEY (`newslike_id`),
+  KEY `FK_Relationship_11` (`news_id`),
+  KEY `FK_Relationship_5` (`user_sid`),
+  CONSTRAINT `FK_Relationship_5` FOREIGN KEY (`user_sid`) REFERENCES `user` (`user_sid`),
+  CONSTRAINT `FK_Relationship_11` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newslike`
+--
+
+LOCK TABLES `newslike` WRITE;
+/*!40000 ALTER TABLE `newslike` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newslike` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `name` varchar(50) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  `biography` varchar(200) DEFAULT NULL,
+  `profilepic` varchar(200) DEFAULT NULL,
+  `gender` enum('男','女') DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `preferencegens` double DEFAULT NULL,
+  `selfgens` double DEFAULT NULL,
+  `user_sid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`user_sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-08-17 19:44:56
